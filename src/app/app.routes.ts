aba713801412581d96f505d14dc110647daa90c6
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-05-20 16:36:40 +0200

    Start project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Pipes básicos',
+    loadComponent: () => import('./pages/basic-page/basic-page'),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes personalizados',
+    loadComponent: () => import('./pages/custom-page/custom-page'),
+  },
+  {
+    path: 'numbers',
+    title: 'Números',
+    loadComponent: () => import('./pages/numbers-page/numbers-page'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes poco comunes',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic',
+  },
+];
