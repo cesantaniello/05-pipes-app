522cc4e74b1c36065aa5a39c1bda5678ada825fe
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-05-21 11:48:24 +0200

    Decimal, percent and currency pipes

--- src/app/pages/numbers-page/numbers-page.ts
+++ src/app/pages/numbers-page/numbers-page.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './numbers-page.html',
 })
-export default class NumbersPage {}
+export default class NumbersPage {
+  totalSells = signal(1234567890);
+  percentage = signal(0.123456789);
+}
