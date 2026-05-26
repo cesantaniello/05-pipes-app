7f7b9a95a40380f967458e07e4bff2642ec772b6
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-05-26 10:54:50 +0200

    Create KeyValuePipe

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { Card } from "../../components/card/card";
-import { I18nPluralPipe, I18nSelectPipe, JsonPipe, SlicePipe, UpperCasePipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 const client1 = {
   name: 'Pedro',
@@ -19,7 +19,7 @@ const client2 = {
 @Component({
   selector: 'app-uncommon-page',
   imports: [Card, I18nSelectPipe, I18nPluralPipe, SlicePipe,
-    JsonPipe, UpperCasePipe],
+    JsonPipe, UpperCasePipe, KeyValuePipe, TitleCasePipe],
   templateUrl: './uncommon-page.html',
 })
 export default class UncommonPage {
@@ -45,4 +45,11 @@ export default class UncommonPage {
   );
 
   deleteClient() { this.clients.update(prev => prev.slice(1)); }
+
+  // KeyValuePipe
+  person = signal({
+    name: 'Pedro',
+    age: 35,
+    address: 'Calle 123, Ciudad',
+  });
 }
