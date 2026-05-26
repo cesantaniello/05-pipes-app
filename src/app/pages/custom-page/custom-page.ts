ceeb342be65b920f2a3bbce3d8368be3c3604054
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-05-26 16:29:57 +0200

    Create CustomPipe with ToggleCase

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [ToggleCasePipe],
   templateUrl: './custom-page.html',
 })
-export default class CustomPage {}
+export default class CustomPage {
+  name = signal('Pedro');
+  upperCase = signal(true);
+}
