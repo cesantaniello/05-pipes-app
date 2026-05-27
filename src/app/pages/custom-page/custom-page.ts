24f7cfbdd4a34f7f2f0774df13e92cf35244a6e8
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-05-27 16:15:35 +0200

    CustomPipe with HeroTextColor

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -2,11 +2,15 @@ import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
-import { HeroColorPipe } from '../../pipes/heroColor-pipe';
+import { HeroColorPipe } from '../../pipes/heroColor.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe,
+    HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe],
   templateUrl: './custom-page.html',
 })
 export default class CustomPage {
