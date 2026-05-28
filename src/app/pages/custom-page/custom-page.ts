19f75c47130f9b9dd0cba17d7c8ecf198c6419b7
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-05-28 10:31:17 +0200

    CustomPipe with HeroFilterPipe

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -8,18 +8,23 @@ import { TitleCasePipe } from '@angular/common';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
 import { Hero } from '../../interfaces/hero.interfaces';
+import { HeroFilterPipe } from '../../pipes/hero-filter.pipe';
 
 @Component({
   selector: 'app-custom-page',
   imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe,
-    HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe, HeroSortByPipe],
+    HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe,
+    HeroSortByPipe, HeroFilterPipe],
   templateUrl: './custom-page.html',
 })
 export default class CustomPage {
   name = signal('Pedro');
+
   upperCase = signal(true);
 
   heroes = signal(heroes);
 
   sortBy = signal<keyof Hero | null>(null);
+
+  searchQuery = signal('');
 }
